cef437644310780b8670d2fed8959e0216415451
Oluwatobi Fatumo <tobyfatumo@gmail.com>
2025-03-12 21:18:17 +0100

    clerk and layout update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,14 @@
 // app/layout.tsx
 import "./globals.css";
 import { ReactNode } from "react";
+import { ClerkProvider } from "@clerk/nextjs";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
